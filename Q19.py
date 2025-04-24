8c61533b22681bef838c0ff448e7d66a638c3488
Jaiwish77 <jaiwishpandya1@gmail.com>
2025-04-24 22:46:01 +0530

    Delete Q19.py

--- Q19.py
+++
@@ -1,6 +0,0 @@
-def recursive_length(s):
-    if s == "":
-        return 0
-    return 1 + recursive_length(s[1:])
-str=input("Enter a string: ")
-print("Length of the string is",recursive_length(str))
