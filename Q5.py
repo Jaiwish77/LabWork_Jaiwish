04e8400190f80889a73de8246537bc22a2d492c6
Jaiwish77 <jaiwishpandya1@gmail.com>
2025-04-24 22:47:20 +0530

    Delete Q5.py

--- Q5.py
+++
@@ -1,9 +0,0 @@
-def ispangram(s):
-    charset=set(s.lower())
-    alphaset=set("abcdefghijklmnopqrstuvwxyz")
-    return charset>=alphaset
-s=input("Enter a string: ")
-if ispangram(s):
-    print("The string is a pangram.")
-else:
-    print("The string is not a pangram.")
